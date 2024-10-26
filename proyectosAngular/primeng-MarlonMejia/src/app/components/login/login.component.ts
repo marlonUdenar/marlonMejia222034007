4a12d1ef6eb1d6998fb76406da305963fe816349
mm <marlonudenar@gmail.com>
2024-10-26 11:07:10 -0500

    calse: feat login

--- proyectosAngular/primeng-MarlonMejia/src/app/components/login/login.component.ts
+++ proyectosAngular/primeng-MarlonMejia/src/app/components/login/login.component.ts
@@ -3,20 +3,20 @@ import { CardModule } from 'primeng/card';
 import "../../formularios/formularios-plantilla1/formularios-plantilla1.component";
 import { FormulariosPlantilla1Component } from '../../formularios/formularios-plantilla1/formularios-plantilla1.component';
 import { ButtonModule } from 'primeng/button';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PasswordModule } from 'primeng/password';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CardModule, FormulariosPlantilla1Component, ButtonModule, 
-    ReactiveFormsModule, PasswordModule, FormsModule],
+    ReactiveFormsModule, PasswordModule, FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  loginForm;
+  loginForm: FormGroup;
 
   constructor(private fb: FormBuilder){
 
@@ -38,13 +38,17 @@ export class LoginComponent {
       }],
   
     });
+
+    
   }
 
-  get email() {
-    return this.loginForm.controls['email'];
-  }
+  onSubmit() {
+    if(this.loginForm.validator) {
+      console.log(this.loginForm.value);
+    } else {
+      console.log('Formulario Invalido')
+    }
+  };
 
-  get password() {
-    return this.loginForm.controls['password'];
-  }
+
 }
